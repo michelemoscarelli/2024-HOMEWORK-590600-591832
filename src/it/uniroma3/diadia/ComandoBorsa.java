e0ff93168a2d9749955db5c53f027b9a751b34cb
Valexios <125564845+Valexios@users.noreply.github.com>
2024-04-24 17:34:18 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/ComandoBorsa.java
@@ -0,0 +1,11 @@
+package it.uniroma3.diadia;
+
+public class ComandoBorsa implements Comandoi{
+
+	@Override
+	public void esegui(Partita partita) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/it/uniroma3/diadia/FabbricaDiComandi.java
@@ -0,0 +1,10 @@
+package it.uniroma3.diadia;
+
+public class FabbricaDiComandi {
+
+	public Comandoi costruisciComando(String istruzione) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
