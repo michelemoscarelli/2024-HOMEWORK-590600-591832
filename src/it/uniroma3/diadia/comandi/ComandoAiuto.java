7b98d311759d835ce3ee62c5622161707b0ab322
Valexios <125564845+Valexios@users.noreply.github.com>
2024-04-25 12:57:12 +0200

    Add files via upload

--- src/it/uniroma3/diadia/comandi/ComandoAiuto.java
+++ src/it/uniroma3/diadia/comandi/ComandoAiuto.java
@@ -1,6 +1,7 @@
 package it.uniroma3.diadia.comandi;
 
 import it.uniroma3.diadia.IO;
+import it.uniroma3.diadia.IOConsole;
 import it.uniroma3.diadia.Partita;
 
 /**
@@ -9,13 +10,14 @@ import it.uniroma3.diadia.Partita;
 public class ComandoAiuto implements Comando{
 	
 	static final private String[] elencoComandi = {"vai", "borsa", "prendi", "posa", "guarda", "aiuto", "fine"};
-	private IO io;
+	private IO io= new IOConsole();
 	
 	@Override
 	public void esegui(Partita partita) {
+		StringBuilder aiuto = new StringBuilder();
 		for(int i=0; i< elencoComandi.length; i++) 
-			this.io.mostraMessaggio(elencoComandi[i]+" ");
-		this.io.mostraMessaggio("");
+			aiuto.append(elencoComandi[i]+" ");
+		this.io.mostraMessaggio(aiuto.toString());
 		
 	}
 
