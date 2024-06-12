2a8f9fa067d5ec67cff77a62e2d696e8fb16f423
Valexios <125564845+Valexios@users.noreply.github.com>
2024-06-12 18:57:36 +0200

    Add files via upload

--- src/it/uniroma3/diadia/comandi/ComandoVai.java
+++ src/it/uniroma3/diadia/comandi/ComandoVai.java
@@ -1,7 +1,9 @@
 package it.uniroma3.diadia.comandi;
 
 import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.ambienti.Direzione;
 import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.giocatore.Giocatore;
 
 /**
  * Cerca di andare in una direzione. Se c'e' una stanza ci entra 
@@ -9,40 +11,44 @@ import it.uniroma3.diadia.ambienti.Stanza;
  */
 public class ComandoVai extends AbstractComando {
 
-	private String direzione;
+	private Direzione direzione;
 
 
 
 	/**
 	 * esecuzione del comando
 	 */
+	
 	@Override
 	public void esegui(Partita partita) {
+		Stanza stanzaCorrente = partita.getLabirinto().getStanzaCorrente();
 		Stanza prossimaStanza = null;
-
-		if(direzione==null) {
-			this.getIo().mostraMessaggio("Dove vuoi andare ? Devi specificare una direzione");
-			return;
+		if (this.getParametro() == null) {
+			this.getIo().mostraMessaggio("Dove vuoi andare? Devi specificare una direzione");
 		}
-
-
-		prossimaStanza = partita.getLabirinto().getStanzaCorrente().getStanzaAdiacente(direzione);
-		if (prossimaStanza == null) {
+		if(this.getParametro()!=null )// && (Direzione.valueOf(this.getParametro()).getClass() != Direzione.class))
+			try {
+			prossimaStanza = stanzaCorrente.getStanzaAdiacente(this.direzione);
+			} catch(IllegalArgumentException e) {
+				this.getIo().mostraMessaggio("Direzione inesistente");
+				return;
+			}
+			
+			if (prossimaStanza == null) {
 			this.getIo().mostraMessaggio("Direzione inesistente");
 			return;
 		}
 
-		else {
 			partita.getLabirinto().setStanzaCorrente(prossimaStanza);
-			partita.getGiocatore().setCfu(partita.getGiocatore().getCfu()-1);
-			this.getIo().mostraMessaggio(partita.getLabirinto().getStanzaCorrente().getDescrizione());
-		}		 
+		this.getIo().mostraMessaggio(partita.getLabirinto().getStanzaCorrente().getNome());
+		Giocatore giocatore = partita.getGiocatore();
+		giocatore.setCfu(giocatore.getCfu() - 1);
 	}
 
 
 	@Override
 	public void setParametro(String direzione) {
-		this.direzione = direzione;
+		this.direzione = Direzione.valueOf(direzione);
 
 	}
 
@@ -55,7 +61,7 @@ public class ComandoVai extends AbstractComando {
 
 	@Override
 	public String getParametro() {
-		return this.direzione;
+		return this.direzione.toString();
 	}
 	
 }
